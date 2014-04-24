0ec065a0ec58164bd4e9df414b51677dc4e1da37
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-04-23 17:06:50 -0700

    changed line of parser

--- hgnc_parser.py
+++ hgnc_parser.py
@@ -28,8 +28,7 @@ for line in f:
 
     # ensure the column has at least one value before printing
     if len(columns) > 2:
-    	sql = "INSERT INTO gene_fam (gene, family_name, family_abrev) VALUES " + 
-    		"('%s', '%s', '%s')" % (columns[3], columns[2], columns[1]) 
+    	sql = "INSERT INTO gene_fam (gene, family_name, family_abrev) VALUES " + "('%s', '%s', '%s')" % (columns[3], columns[2], columns[1]) 
 
     try:
     	cursor.execute(sql)
