33ec05220af1ba32d4f75f254aaf9f7390084089
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-04-30 14:31:38 -0700

    test

--- gene_locations.py
+++ gene_locations.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python
 import cgi
 print "Content-type: text/html\n\n"
-print "<html> Hello world!</html>" 
+
+
+
+print "<html> Hello world!</html>"
 
 #Script to find locations of genes in genome
 #input: list of info about the selected gene, list of info about gene family
 #ouput: json or tsv file 
-
+'''
 import MySQLdb
 import Bio
 import os
 from Bio import Entrez
 import json
-
+'''
 #store the locations
