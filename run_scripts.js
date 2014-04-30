55bcf38db548210e94c511596c043309dd729a8a
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-04-30 16:04:07 -0700

    test

--- run_scripts.js
+++ run_scripts.js
@@ -1,10 +1,12 @@
  $(function() {
            $('#run-python').click(function(e){
-               var url = '192.168.56.101/Gene_Family_Visualization/gene_locations.py';
+               var url = '/Gene_Family_Visualization/gene_locations.py';
                $.ajax({
                        type: "GET",
                        url: url,
                }).done(function(results){
+                      alert("fetch this content");
+                      console.log(results)
                        $('#ajax-div').empty().append(results);
                });
           });
