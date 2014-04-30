21d63145811a6e86062e4d2b51b0ba87b6a582c2
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-04-30 14:27:42 -0700

    testing

--- gene_locations.py
+++ gene_locations.py
@@ -1,6 +1,9 @@
-#Script to find locations of genes in genome
-# ~/anaconda/bin/python
+#!/usr/bin/python
+import cgi
+print "Content-type: text/html\n\n"
+print "<html> Hello world!</html>" 
 
+#Script to find locations of genes in genome
 #input: list of info about the selected gene, list of info about gene family
 #ouput: json or tsv file 
 
