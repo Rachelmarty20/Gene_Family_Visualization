1267944689d2f746506965edb83c8bc4d20f1814
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-04-30 19:21:58 -0700

    updating file paths

--- run_scripts.js
+++ run_scripts.js
@@ -1,6 +1,6 @@
  $(function() {
            $('#run-python').click(function(e){
-               var url = '/Gene_Family_Visualization/gene_locations.py';
+               var url = '/Gene_Family_Visualization/bin/gene_locations.py';
                $.ajax({
                        type: "GET",
                        url: url,
