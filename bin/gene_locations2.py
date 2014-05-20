3771a9bb8a54156ab1fb5a4cce89c8461cf47bf1
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-05-19 18:53:51 -0700

    transcript data from all genes in family

--- bin/gene_locations2.py
+++ bin/gene_locations2.py
@@ -324,7 +324,7 @@ def get_seqs(gene):
 			#create loop for seq; maybe two for nuc and aa
 			for i in seqs:
 				#keep a counter to know numbers of these nodes to link them
-				node.append({'name':(i[0] + " transcript"), 'size':(i[3]*10), 'chromosome':i[1]})
+				node.append({'name':(i[0] + " transcript"), 'size':(i[3]*300), 'chromosome':i[1]})
 				link.append({'source':tracker[i[0]], 'target':(node_num), 'value':(3)})
 				#node.append({'name':(i[0] + "protein"), 'size':(i[5]*10), 'chromosome':i[1]})
 				#link.append({'source':(node_num), 'target':(node_num + 1), 'value':(3)})
