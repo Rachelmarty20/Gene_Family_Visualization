8a3a7d14ca033a9e59da2ce9ea484dfb74023487
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-05-21 16:05:02 -0700

    adding emboss capabilities

---
+++ bin/pairwise.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import os
+import sys
+import subprocess
+import Bio
+from Bio.Emboss.Applications import WaterCommandline
+cmd = WaterCommandline(gapopen=10, gapextend=0.5)
+cmd.asequence = "asis:ACCCGGGCGCGGT"
+cmd.bsequence = "asis:ACCCGAGCGCGGT"
+cmd.outfile = "temp_water.txt"
+#cline.outfile = sys.stdout
+print cmd
+stdout, stderr = cmd()
+#cmd
