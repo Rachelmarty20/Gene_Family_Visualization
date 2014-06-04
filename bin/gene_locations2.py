14cf0dd6c0a5394b399846eb871bf1414465ac7a
Rachelmarty20 <Rachelmarty20@gmail.com>
2014-06-04 08:14:09 -0700

    updating database calls

--- bin/gene_locations2.py
+++ bin/gene_locations2.py
@@ -27,8 +27,8 @@ mygene = form.getvalue("mygene")
 
 
 def get_seqs(gene):
-	#connect to db
-	db = MySQLdb.connect("localhost","root","Bimm185", "rmarty")
+	#connect to db  "localhost","root","quaker22","gene" 
+	db = MySQLdb.connect("localhost","rmarty","Bimm185", "rmarty")
 
 	#setup cursor
 	cursor = db.cursor()
